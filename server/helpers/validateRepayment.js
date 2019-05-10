11c119df1341263b1c9b6b67ff134b5a4bcfb5cd
Alexis <alexismajyambere2@gmail.com>
2019-05-10 10:20:25 +0200

    done

--- server/helpers/validateRepayment.js
+++ server/helpers/validateRepayment.js
@@ -5,7 +5,7 @@ exports.repayValidator=(repay)=>{
 
 	const repayFormat= {
 		loanId:jo.number().required(),
-	    amount: jo.number().required(),  
+	    amount: jo.number().positive().allow(0).required() 
 
   };
   return jo.validate(repay, repayFormat);
