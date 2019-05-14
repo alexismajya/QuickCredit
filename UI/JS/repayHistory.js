3995f65089b7e4c9d94b4e70f2c90ab3f1a1aecf
Alexis <alexismajyambere2@gmail.com>
2019-05-14 03:18:11 +0200

    [ft_#165987493] Working on LFA feedback

--- UI/JS/repayHistory.js
+++ UI/JS/repayHistory.js
@@ -12,9 +12,7 @@ const tablecells=(loanId,date,Minstall,amount)=>{
 const viewLoans=()=>{
 	var UserArray=[];
 	document.getElementById('RepaymentsTable');
-		//if(localStorage.Repayments){
-		//UserArray=JSON.parse(localStorage.Repayments);
-		//}
+		
 		UserArray=[{Id:1,createdOn:"4/4/2019",monthkyInstallment:"40000",amount:"40000"}];
 		for(var i=0; i<UserArray.length;i++){
 			tablecells(UserArray[i].Id,UserArray[i].createdOn,UserArray[i].monthkyInstallment,UserArray[i].amount);
