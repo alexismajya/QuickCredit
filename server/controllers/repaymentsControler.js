840aa751f0b1bef3d109c46f89cb08ec2697f610
Alexis <alexismajyambere2@gmail.com>
2019-05-19 10:35:00 +0200

    [ch-#166034071] tesing coverage

--- server/controllers/repaymentsControler.js
+++ server/controllers/repaymentsControler.js
@@ -11,7 +11,7 @@ const repaymentsCont={
          if (!repaymentsMod.allrepays) 
             return res.status(404).json({status: 404, error: 'No repayments(s) found' });
 
-         return res.status(200).json({status:200, data: repaymentsMod.allrepays, message:"Data found"});
+         return res.status(200).json({status:200, data: repaymentsMod.allrepays(req.body), message:"Data found"});
    },
     repayLoan: (req, res) => {
         // Validating 
@@ -22,21 +22,21 @@ const repaymentsCont={
         }
         else{
            
-            const loanTorepay = loansMod.loans.find(l => l.id === req.query.loanId && l.repaid!="true")
+            const loanTorepay = loansMod.loans.find(l => l.id === parseInt(req.params.loanId) && l.repaid!="true")
 
              if (!loanTorepay) {
                 return res.status(400).json({ status: 400, error: 'The specified unrepaid loan is not found !!' });
             }
             else{
                 
-                let newrepayloan = repaymentsMod.repayments.find(nl => nl.loanId === req.query.loanId);
+                let newrepayloan = repaymentsMod.repayments.find(nl => nl.loanId === parseInt(req.params.loanId));
 
                 if (newrepayloan){ 
                      return res.status(400).json({ status: 400, error: 'This installment is already recorded' });
                 }
                 else{
                           
-                     newrepayloan= repaymentsMod.repayLoan(req.body,re.query.loanId,res);
+                     newrepayloan= repaymentsMod.repayLoan(req.body,parseInt(req.params.loanId),res);
                         
                         res.status(201).json({status:201,message:"The loan repayment was successfully recorded", data: newrepayloan});
                 }
@@ -46,7 +46,7 @@ const repaymentsCont={
     },
 
     repaymentsHistory: (req, res) => {
-        const myrepayments = repaymentsMod.repayments.find(l => l.loanId === req.query.loanId)
+        const myrepayments = repaymentsMod.repayments.find(l => l.loanId === parseInt(req.params.loanId));
 
         if (!myrepayments) 
            return res.status(404).json({status: 404, error: 'No repayments history found' });
